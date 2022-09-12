c0b5075a0ae9056e5d99dea95379aadf74da76cc
kirti Chaand <99725736+kirti-kk@users.noreply.github.com>
2022-09-12 17:53:44 +0530

    Create mailchimp.js

---
+++ mailchimp.js
@@ -0,0 +1,18 @@
+
+// select the target node
+var target = document.getElementById('mce-success-response');
+
+// create an observer instance
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    if (target.innerHTML === "Thank you for subscribing!") {
+      target.innerHTML = "Check your inbox for your shopping promo code!";
+    }
+  });
+});
+
+// configuration of the observer:
+var config = { attributes: true, childList: true, characterData: true };
+
+// pass in the target node, as well as the observer options
+observer.observe(target, config);
